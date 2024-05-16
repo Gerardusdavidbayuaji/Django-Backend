28966f09068901609b16d2850a5649368c30aa0a
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-05-16 12:30:27 +0700

    add: update feature upload

--- backend/fews/views.py
+++ backend/fews/views.py
@@ -1,29 +1,55 @@
+import os
 import zipfile
 from django.shortcuts import render, redirect
 from .forms import UploadFileForm
-from .models import UploadedFile
-from django.core.files.storage import FileSystemStorage
-import os
 
-def handle_uploaded_file(f):
-    upload_dir = 'backend/fews/repository'
-    if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
-    with open(os.path.join(upload_dir, f.name), 'wb+') as destination:
+UPLOAD_DIR = 'backend/fews/repository'
+MAX_FILE_SIZE = 200 * 1024 * 1024  # maksimal upload data ukuran 200 MB
+
+def extract_zip(zip_file, destination):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(destination)
+
+def handle_zip_file(f, upload_dir):
+    extract_dir = os.path.join(upload_dir, os.path.splitext(f.name)[0])
+    os.makedirs(extract_dir, exist_ok=True)
+    extract_zip(f, extract_dir)
+    return True
+
+def handle_tif_file(f, upload_dir):
+    tif_file_path = os.path.join(upload_dir, f.name)
+    if os.path.exists(tif_file_path):
+        os.remove(tif_file_path)
+    with open(tif_file_path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    return True
 
-    # Extract the zip file
-    with zipfile.ZipFile(os.path.join(upload_dir, f.name), 'r') as zip_ref:
-        zip_ref.extractall(upload_dir)
+def handle_uploaded_file(f):
+    if f.size > MAX_FILE_SIZE:
+        return False
+    
+    try:
+        if f.name.endswith('.zip'):
+            return handle_zip_file(f, UPLOAD_DIR)
+        elif f.name.endswith('.tif'):
+            return handle_tif_file(f, UPLOAD_DIR)
+    except Exception as e:
+        print(f"Error handling file: {e}")
+        return False
+
+    return False  # tidak support type data
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            form.save()
-            return redirect('upload_success')
+            uploaded_file = request.FILES['file']
+            if handle_uploaded_file(uploaded_file):
+                return redirect('upload_success')
+            else:
+                error_message = "Failed to handle the uploaded file."
+                return render(request, 'upload.html', {'form': form, 'error_message': error_message})
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
